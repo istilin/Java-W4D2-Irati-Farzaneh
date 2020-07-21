d8d67bc23d3c274e68920ef1cee6458789218bc0
Farzaneh Younesi <farzanehynsi@gmail.com>
2020-07-21 11:46:49 +0200

    Done

---
+++ src/Farzaneh/A1/Main.java
@@ -0,0 +1,20 @@
+package Farzaneh.A1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] myNumbers = {1, 2, 3};
+
+
+        try {
+            System.out.println(myNumbers[10]);
+        } catch (Exception x) {
+            System.out.println("Something wrong");
+        }
+
+
+
+        System.out.println("Amin khare gave nare ");
+    }
+
+}
